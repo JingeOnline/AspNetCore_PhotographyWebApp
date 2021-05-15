ee2642579be8d36209fe7a899d25f01dae8655b6
Jinge Zhang <jinge_windows@hotmail.com>
2021-05-16 00:55:08 +1000

    添加项目文件。

---
+++ PhotographyWebAppCore/Components/SliderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotographyWebAppCore.Controllers
+{
+    public class SliderViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
